15ef6fc35dc476ff24117011857560ee7c93523a
Mahesh Reghunath <mahesh@Mahesh-Desktop.(none)>
2013-06-11 13:05:02 +0530

    Modified

--- src/com/selenium/googlesearch/GoogleSearch.java
+++ src/com/selenium/googlesearch/GoogleSearch.java
@@ -12,52 +12,73 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class GoogleSearch  {
-    
-	@Test
-	public static void search() throws MalformedURLException {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-		DesiredCapabilities capabilities = null;
+public class GoogleSearch {
+
+	DesiredCapabilities capabilities = null;
+
+	static RemoteWebDriver driver;
+
+	@Parameters({ "node" })
+	@BeforeClass
+	public void setUp(@Optional("http://10.2.0.93:5555/wd/hub") String node)
+			throws MalformedURLException {
 
 		capabilities = DesiredCapabilities.firefox();
 		capabilities.setPlatform(Platform.ANY);
-		capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT,
-				false);
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:5555/wd/hub"), capabilities);
+		capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, false);
+		driver = new RemoteWebDriver(new URL(node), capabilities);
+	}
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+	@Test
+	public static void search() throws MalformedURLException {
+		// Create a new instance of the Firefox driver
+		// Notice that the remainder of the code relies on the interface,
+		// not the implementation.
+		/*
+		 * DesiredCapabilities capabilities = null;
+		 * 
+		 * capabilities = DesiredCapabilities.firefox();
+		 * capabilities.setPlatform(Platform.ANY);
+		 * capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, false);
+		 * RemoteWebDriver driver = new RemoteWebDriver(new
+		 * URL("http://localhost:5555/wd/hub"), capabilities);
+		 */
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+		// And now use this to visit Google
+		driver.get("http://www.google.com");
+		// Alternatively the same thing can be done like this
+		// driver.navigate().to("http://www.google.com");
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
+		// Find the text input element by its name
+		WebElement element = driver.findElement(By.name("q"));
 
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+		// Enter something to search for
+		element.sendKeys("Cheese!");
 
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
+		// Now submit the form. WebDriver will find the form for us from the
+		// element
+		element.submit();
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        //Close the browser
-        //driver.quit();
-    }
+		// Check the title of the page
+		System.out.println("Page title is: " + driver.getTitle());
+
+		// Google's search is rendered dynamically with JavaScript.
+		// Wait for the page to load, timeout after 10 seconds
+		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				return d.getTitle().toLowerCase().startsWith("cheese!");
+			}
+		});
+
+		// Should see: "cheese! - Google Search"
+		System.out.println("Page title is: " + driver.getTitle());
+
+		// Close the browser
+		// driver.quit();
+	}
 }
